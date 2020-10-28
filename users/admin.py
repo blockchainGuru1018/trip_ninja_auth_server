acccdbb700d84eb8f1bffb9e4f018aada06b8364
YBLee-dev <yblee.biz@gmail.com>
2020-10-28 22:35:59 +0300

    user app admin fixed

--- users/admin.py
+++ users/admin.py
@@ -2,12 +2,3 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from .models import User
-
-
-# class CustomUserAdmin(UserAdmin):
-#     model = User
-#     list_display = ['email', 'username']
-#     list_filter = ('is_admin',)
-#
-#
-# admin.site.register(User, CustomUserAdmin)
