8688da593fca75bb91cbadcf034c7344160f4a2a
Rob D <rob.dumont99@gmail.com>
2021-01-06 11:03:35 -0500

    Update users/models.py
    Co-authored-by: Kieran <kieran.mcclymont@tripninja.io>

--- users/models.py
+++ users/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
-from django.contrib.auth.models import UserManager
 from common.models import CommonParameters
 from teams.models import Agency, Team
 
