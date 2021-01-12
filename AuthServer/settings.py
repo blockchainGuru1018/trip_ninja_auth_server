53382f29c3afbee692dff4a5bc061d2604249261
andrescollart <47606192+andrescollart@users.noreply.github.com>
2021-01-12 10:20:31 -0400

    enablex x-ray and removed db migrate

--- AuthServer/settings.py
+++ AuthServer/settings.py
@@ -42,6 +42,14 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
+    'aws_xray_sdk.ext.django.middleware.XRayMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'AuthServer.urls'
