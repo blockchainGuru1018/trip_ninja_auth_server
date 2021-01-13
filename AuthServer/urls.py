8690c7b11cd6c170f4b95ac5171257b7ee9fd32d
andrescollart <47606192+andrescollart@users.noreply.github.com>
2021-01-13 10:37:36 -0400

    added health check

--- AuthServer/urls.py
+++ AuthServer/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r"^api/v1/", include('api.urls')),
+    url(r'^health/?', include('health_check.urls')),
 ]
 
 if settings.DEBUG:
