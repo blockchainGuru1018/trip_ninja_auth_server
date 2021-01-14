d0c6ddf2ffeb70d38f18dec665f9d7176c176043
k.mcclymont <kieran.mcclymont@tripninja.io>
2021-01-14 11:26:26 -0400

    - removed unnecessary import

--- QuickTrip/settings.py
+++ QuickTrip/settings.py
@@ -87,11 +87,11 @@ FIXTURE_DIRS = ['fixtures']
 DATABASES = {
     "default": {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'trip_ninja',
-        'USER': 'kieranmcclymont',
-        'PASSWORD': '12345',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
